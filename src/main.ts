bc8e0e4a9beadcc2057b68fe3fd72c951bb6fb1d
anci.shih <anci.shih@ytec.com.tw>
2024-09-06 18:03:27 +0800

    add dscfm page

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ const router = createRouter({
   routes:[
     { path:`${import.meta.env.MODE == "development" ? "": "/CFM"}/Home/CFMEditor`, component: () => import('./pages/Config.vue') },
     //{ path:`${import.meta.env.MODE == "development" ? "": "/CFM"}/Home/CFMEditor1`, component: () => import('./pages/Config.vue') },
-    //{ path:`${import.meta.env.MODE == "development" ? "": "/CFM"}/Home/DSCFM`, component:() => import('./pages/DScfm.vue') },
+    { path:`${import.meta.env.MODE == "development" ? "": "/CFM"}/Home/CFMEditor1`, component:() => import('./pages/DScfm.vue') },
     // { path:`${import.meta.env.MODE == "development" ? "": "/CFM"}/Home/DSCFM`, component:() => import('./pages/TESTcfm.vue') }
     // { path:`${import.meta.env.MODE == "development" ? "": "/CFM"}/Home/CFMEditor1`, component: () => import('./pages/Config.vue') },
   ]
